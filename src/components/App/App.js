70b6cf35a2daed442e3cd4e66d1e9ff203bf406d
vadimshift <vadimshift@yandex.ru>
2021-09-19 12:05:01 +0500

    создал компонент Header

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div className='page'>
+      <div className='page__container'>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import logo from "../../images/logo.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Логотип проекта" />
+      <h1 className="header__title">Jet Style Book</h1>
+    </header>
+  );
+}
+
+export default Header;
